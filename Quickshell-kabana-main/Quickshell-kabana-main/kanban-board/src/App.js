7e7d7103c94e3ed84310aff88c7ed47c6c249b3d
yash87234 <149461520+yash87234@users.noreply.github.com>
2023-10-31 11:25:25 +0530

    Add files via upload

---
+++ Quickshell-kabana-main/Quickshell-kabana-main/kanban-board/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "./actions/action";
+import { useEffect } from "react";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { tickets } = useSelector((state) => state.dataSlice);
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
+  return tickets ? (
+    <div>
+      <Navbar />
+      <Dashboard />
+    </div>
+  ) : (
+    console.error("Something went wrong")
+  );
+};
+
+export default App;
